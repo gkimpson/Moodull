4e1a41732c9cf9b077fda1e19fb6fd040fbb7ef7
Gavin Kimpson <gkimpson@gmail.com>
2023-02-02 23:28:01 +0000

    User factory & db seeder added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,9 +18,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'uuid' => fake()->uuid(),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'gender' => fake()->randomElement(['M', 'F']),
+            'is_active' => fake()->randomElement([0, 1]),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
